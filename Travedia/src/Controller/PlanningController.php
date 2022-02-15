3a194f9f91f8ef2002bb662eb6b7809bc0df9c68
karim mohsen <karim.mohsen@esprit.tn>
2022-02-15 21:21:32 +0100

    FrontOffice
    j'ai realise le crud du front office+templates

--- Travedia/src/Controller/PlanningController.php
+++ Travedia/src/Controller/PlanningController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Utilisateur;
+use App\Entity\Destination;
+use App\Entity\Evenement;
 use App\Entity\Planning;
 use App\Form\PlanningType;
 use App\Repository\PlanningRepository;
+use App\Repository\UtilisateurRepository;
+use App\Repository\DestinationRepository;
+use App\Repository\EvenementRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,6 +36,8 @@ class PlanningController extends AbstractController
      */
     public function addPlanning(Request $request): Response
     {
+
+
         $planning = new Planning();
         $form = $this->createForm(PlanningType::class, $planning);
         $form->handleRequest($request);
@@ -100,5 +108,83 @@ class PlanningController extends AbstractController
 
 
         return $this->redirectToRoute('Planning_show');
+    }
+
+    /**
+     * @param Request $request
+     * @return \symfony\Component_HttpFoundation\RedirectResponse
+     * @Route("/planningFront/add", name="PlanningFront_add")
+     */
+    public function addPlanningFront(Request $request): Response
+    {
+
+
+        $planning = new Planning();
+        $form = $this->createForm(PlanningType::class, $planning);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($planning);
+            $entityManager->flush();
+            return $this->redirectToRoute('PlanningFront_show');
+        }
+
+        return $this->render('planning/addPlanningFront.html.twig', [
+            'planning' => $planning,
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @param PlanningRepository $rep
+     * @return Response
+     * @Route("/planningFront/show",name="PlanningFront_show")
+     */
+    public function showPlanningFront(PlanningRepository $rep)
+    {
+        $planning=$rep->findAll();
+        return $this->render('planning/showPlanningFront.html.twig', [
+            'planning' => $planning,
+        ]);
+    }
+    /**
+     * @param $id
+     * @param PlanningRepository $rep
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|Response
+     * @Route("/planning/editPlanningFront/{id}",name="PlanningFront_edit")
+     */
+    public function editPlanningFront($id,PlanningRepository $rep,Request $request)
+    {
+        $planning=$rep->find($id);
+        $form = $this->createForm(PlanningType::class, $planning);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager=$this->getDoctrine()->getManager();
+            $entityManager->flush();
+
+            return $this->redirectToRoute('PlanningFront_show');
+        }
+
+        return $this->render('planning/editPlanningFront.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    /**
+     * @param $id
+     * @param PlanningRepository $rep
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse
+     * @Route("/planning/delPlanningFront/{id}",name="PlanningFront_del")
+     */
+    public function delPlanningFront($id,PlanningRepository $rep)
+    {
+        $planning=$rep->find($id);
+        $entityManager=$this->getDoctrine()->getManager();
+        $entityManager->remove($planning);
+        $entityManager->flush();
+
+
+        return $this->redirectToRoute('PlanningFront_show');
     }
 }
--- Travedia/src/Form/PlanningType.php
+++ Travedia/src/Form/PlanningType.php
@@ -2,7 +2,11 @@
 
 namespace App\Form;
 
+use App\Entity\Destination;
+use App\Entity\Evenement;
 use App\Entity\Planning;
+use App\Entity\Utilisateur;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -24,9 +28,10 @@ class PlanningType extends AbstractType
                 ],
             ])
             ->add('description')
-            ->add('utilisateur')
-            ->add('evenements')
-            ->add('destinations')
+            ->add('utilisateur',EntityType::class,['class'=>Utilisateur::class,'choice_label'=>'nom'])
+            ->add('evenements',EntityType::class,['class'=>Evenement::class,'choice_label'=>'nom','expanded'=>'true','multiple'=>'true'])
+
+            ->add('destinations',EntityType::class,['class'=>Destination::class,'choice_label'=>'nom','expanded'=>'true','multiple'=>'true'])
            // ->add('actualite')
             ->add('Ajouter', SubmitType::class)
         ;
